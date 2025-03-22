1944a81886fc43d4354525dcb0b142c705fb0f2b
tssubhash03 <subhash03ts2004@gmail.com>
2025-03-22 12:17:29 +0530

    Stars effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [stars, setStars] = useState([]);
+
+  useEffect(() => {
+    const starElements = [];
+    const numStars = 300; // Increase number of stars
+
+    for (let i = 0; i < numStars; i++) {
+      const size =
+        Math.random() < 0.85 ? Math.random() * 2 + 1 : Math.random() * 4 + 2; // 85% small, 15% big
+      const twinkleSpeed = Math.random() * 3 + 2 + "s"; // Different twinkle speeds
+      const top = Math.random() * 100 + "vh";
+      const left = Math.random() * 100 + "vw";
+
+      starElements.push({ id: i, top, left, size: size + "px", animationDuration: twinkleSpeed });
+    }
+
+    setStars(starElements);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {stars.map((star) => (
+        <div
+          key={star.id}
+          className="star"
+          style={{
+            top: star.top,
+            left: star.left,
+            width: star.size,
+            height: star.size,
+            animationDuration: star.animationDuration,
+          }}
+        ></div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
